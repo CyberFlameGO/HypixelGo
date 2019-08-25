717eb667661830da2db291c74ada7e4200c6e6b9
George <george.marr.cobc@gmail.com>
2019-08-25 22:10:21 +0100

    add in a base test

--- main/Hypixel.go
+++ main/Hypixel.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 const apiUrl string = "https://api.hypixel.net/"
 
 func main() {
 
+	resp, err := http.Get(apiUrl)
+
+	if err != nil {
+		fmt.Errorf("Could not send request, got error: %s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(body)
 }
