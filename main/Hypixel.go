580f2418c775d2a5690209042984538e6b3f411e
George <george.marr.cobc@gmail.com>
2019-08-25 22:18:51 +0100

    well that was hard

--- main/Hypixel.go
+++ main/Hypixel.go
@@ -36,28 +36,60 @@ func sendRequest(apiType string, apiKey string, uuid string) {
 
 	case "guild":
 		{
+			strings.Replace(apiKey, "[#type]", "guild", 1)
 
+			resp, err := http.Get(apiUrl)
+
+			if err != nil {
+				fmt.Println("Could not sound API request, found error: ", err)
+			}
+
+			fmt.Println(resp)
 		}
 
 		break
 
 	case "keys":
 		{
+			strings.Replace(apiKey, "[#type]", "keys", 1)
 
+			resp, err := http.Get(apiUrl)
+
+			if err != nil {
+				fmt.Println("Could not sound API request, found error: ", err)
+			}
+
+			fmt.Println(resp)
 		}
 
 		break
 
 	case "boosters":
 		{
+			strings.Replace(apiKey, "[#type]", "boosters", 1)
 
+			resp, err := http.Get(apiUrl)
+
+			if err != nil {
+				fmt.Println("Could not sound API request, found error: ", err)
+			}
+
+			fmt.Println(resp)
 		}
 
 		break
 
 	case "friends":
 		{
+			strings.Replace(apiKey, "[#type]", "friends", 1)
 
+			resp, err := http.Get(apiUrl)
+
+			if err != nil {
+				fmt.Println("Could not sound API request, found error: ", err)
+			}
+
+			fmt.Println(resp)
 		}
 
 		break
